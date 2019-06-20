952758c57b1102fa5b4d68dcefa804933212030f
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-06-21 00:02:40 +0100

    created dropdown menu for hamburger icon (portfolio)

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var navLinks = document.getElementsByClassName("navLink");
+var navLinkVisability = false;
+
+
 hamburgerIcon.onclick = function(){
-  console.log("Hello Jeb");
-};
+  if (navLinkVisability == false){
+    for (var i = 0; i < navLinks.length; i++){
+      navLinks[i].style.display = "block";
+    };
+    navLinkVisability = true;
+  } else {
+    for (var i = 0; i < navLinks.length; i++){
+      navLinks[i].style.display = "none";
+    };
+    navLinkVisability = false;
+  }
+}
