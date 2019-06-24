2fcc3f7f38ece7880aabff5c575fdbf89e418fc1
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-06-24 17:28:08 +0100

    debugged dropdownmenu, added its responsivity

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 var navLinks = document.getElementsByClassName("navLink");
 var navLinkVisability = false;
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+var timeout = true;
+var dropDownStatus = false;
 
+function setVisability(element, status) {
+  for (var i = 0; i < element.length; i++){
+    element[i].style.display = status;
+  };
+}
 
 hamburgerIcon.onclick = function(){
   if (navLinkVisability == false){
-    for (var i = 0; i < navLinks.length; i++){
-      navLinks[i].style.display = "block";
-    };
+    setVisability(navLinks, "block");
     navLinkVisability = true;
+    dropDownStatus = true;
   } else {
-    for (var i = 0; i < navLinks.length; i++){
-      navLinks[i].style.display = "none";
-    };
+    setVisability(navLinks, "none")
     navLinkVisability = false;
+    dropDownStatus = false;
   }
 }
+
+window.addEventListener('resize', function() {
+  viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  if (viewportWidth >= 990 && timeout == true) {
+    setVisability(navLinks, "block");
+    navLinkVisability = true;
+    console.log("Large Window");
+    timeout = false;
+    setTimeout(function(){
+      timeout = true;
+    }, 0);
+  } else if (viewportWidth < 990 && timeout == true && dropDownStatus == false) {
+    setVisability(navLinks, "none");
+    navLinkVisability = false;
+    console.log("Small Window");
+    timeout = false;
+    setTimeout(function(){
+      timeout = true;
+    }, 0);
+  } 
+}, false);
