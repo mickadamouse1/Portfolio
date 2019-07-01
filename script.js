a9b0542fda5b54f3a52476bc4db4e6a6bb41efb2
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-07-01 15:40:39 +0100

    debugged drop-down menu, finished

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 var navLinks = document.getElementsByClassName("navLink");
 var navLinkList = document.getElementById("navLinkList");
-var navIcons = document.getElementById("navLinkList").getElementsByClassName("navIconItem")[0];
+var navIcons = document.getElementById("navLinkList").getElementsByClassName("navIconItem");
 var navLinkVisability = false;
 var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 var timeout = true;
 var dropDownStatus = false;
 
 
-// Changes class display by inputting the parameters (element, value)
-function setVisability(element, value) {
+// *CLASS SELECTION FUNCTION* Change display value by inputting the parameters (element, value)
+function setClassVisability(element, value) {
   for (var i = 0; i < element.length; i++){
     element[i].style.display = value;
   };
 }
 
+// *ID SELECTION FUNCTION* Change display value by inputting the parameters (element, value)
 function setIdVisability(variableName, value) {
   variableName.style.display = value;
 }
 
+// *ID CLASS SELECTION FUNCTION* Change display value of all classes within an ID by using parameters (element, value)
+function setAllClassWithinIdVisability(variableName, value){
+  for (i = 0; i < variableName.length; i++) {                         // Loops through to select all elements within this ID
+    variableName[i].style.display = value;
+  };
+}
+
 hamburgerIcon.onclick = function(){
-  if (navLinkVisability == false){
-    setVisability(navLinks, "block");
-    setVisability(navIcons, "inline-block");
+  if (dropDownStatus == false){
+    setClassVisability(navLinks, "block");
+    setAllClassWithinIdVisability(navIcons, "inline-block");                 // set the navigation link visability to block using the class selection function (see above)
+    setIdVisability(navLinkList, "block");
     navLinkVisability = true;
     dropDownStatus = true;
     navFlexContainer.style.padding = "10px 35px 0 35px";
   } else {
-    setVisability(navLinks, "none");
-    setVisability(navIcons, "none");
+    setClassVisability(navLinks, "none");
+    setClassVisability(navIcons, "none");
     navFlexContainer.style.padding = "10px 0 0 0";
     navLinkVisability = false;
     dropDownStatus = false;
@@ -37,29 +46,30 @@ hamburgerIcon.onclick = function(){
 window.addEventListener('resize', function() {
   viewportWidth = window.innerWidth || document.documentElement.clientWidth;
   if (viewportWidth >= 990 && timeout == true) {
-    console.log(dropDownStatus);
-    setVisability(navLinks, "block");
-    setVisability(navIcons, "block");
+    setClassVisability(navLinks, "block");
+    setClassVisability(navIcons, "block");
+    setIdVisability(navLinkList, "inline-flex");
     navLinkVisability = true;
+    console.log(dropDownStatus);
     console.log("Large Window");
     timeout = false;
     setTimeout(function(){
       timeout = true;
     }, 0);
   } else if (viewportWidth < 990 && timeout == true && dropDownStatus == false) {
-    console.log(dropDownStatus);
-    if (dropDownStatus === false) {
-      setVisability(navIcons, "none");
-      setIdVisability(navLinkList, "none");
-    } else {
-      setVisability(navIcons, "inline-block");
-      setIdVisability(navLinkList, "block");
-    };
+    setClassVisability(navIcons, "none");
+    setIdVisability(navLinkList, "none");
     navLinkVisability = false;
+    console.log(dropDownStatus);
     console.log("Small Window");
     timeout = false;
     setTimeout(function(){
       timeout = true;
     }, 0);
+  } else if (viewportWidth < 990 && timeout == true && dropDownStatus == true) {
+    setClassVisability(navIcons, "inline-block");
+    setIdVisability(navLinkList, "block");
+    console.log(dropDownStatus);
+    console.log("Small Window");
   }
 }, false);
