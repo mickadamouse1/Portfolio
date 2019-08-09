d728045ce8f61f4bf4f351a20843f0278eefd374
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-08-09 18:54:24 +0100

    Added Advanced Battleships to Projects

---
+++ projects/advBattleships/script.js
@@ -0,0 +1,160 @@
+var attackScript = document.createElement("script");
+attackScript.src = "attack.js";
+document.head.appendChild(attackScript);
+
+var txtGuesses = document.getElementById("txtGuesses");
+var txtHits = document.getElementById("txtHits");
+var txtOutput = document.getElementById("txtOutput");
+var btnRestart = document.getElementById("btnRestart");
+
+var guesses = 20;
+var hits = 0;
+var gameFinished = false;
+
+var arrCells = [];
+for (var i = 0; i < 64; i++) {
+  arrCells[i] = i + 1
+}
+
+var ship1 = {
+  assignLoc: function() {
+    this.loc1 = generateHorizPos();
+    this.loc2 = this.loc1 + 1;
+  }
+};
+
+var ship2 = {
+  assignLoc: function() {
+    this.loc1 = arrCells[Math.floor(Math.random() * 56)];
+    this.loc2 = this.loc1 + 8;
+    adjustPos();
+  }
+};
+
+function generateHorizPos() {
+  var a = Math.floor(Math.random() * 7) + 1;
+  var b = a + 8;
+  var c = b + 8;
+  var d = c + 8;
+  var e = d + 8;
+  var f = e + 8;
+  var g = f + 8;
+  var h = g + 8;
+  var arr = [a, b, c, d, e, f, g, h];
+  return arr[Math.floor(Math.random() * 8)];
+}
+
+function adjustPos() {
+  if (ship2.loc1 == ship1.loc1 || ship2.loc1 == ship1.loc2 || ship2.loc2 == ship1.loc1 || ship2.loc2 == ship1.loc2) {
+    ship2.loc1 = ship1.loc2 + 1;
+    ship2.loc2 = ship2.loc1 + 8;
+    console.log("moved");
+    if (ship2.loc1 > 54) {
+      ship2.loc1 = ship1.loc1 - 9;
+      ship2.loc2 = ship2.loc1 + 8;
+    }
+  }
+}
+
+function displayShips(shipLoc) {
+  var ship1cell = shipLoc;
+  if (ship1cell < 9) {
+    ship1cell = "a" + ship1cell;
+  } else if (ship1cell < 17) {
+    ship1cell = "b" + (ship1cell - 8);
+  } else if (ship1cell < 25) {
+    ship1cell = "c" + (ship1cell - 16);
+  } else if (ship1cell < 33) {
+    ship1cell = "d" + (ship1cell - 24);
+  } else if (ship1cell < 41) {
+    ship1cell = "e" + (ship1cell - 32);
+  } else if (ship1cell < 49) {
+    ship1cell = "f" + (ship1cell - 40);
+  } else if (ship1cell < 57) {
+    ship1cell = "g" + (ship1cell - 48);
+  } else if (ship1cell <= 64) {
+    ship1cell = "h" + (ship1cell - 56);
+  }
+  document.getElementById(ship1cell.toString()).classList.add("ship");
+}
+
+function restartGame() {
+  guesses = 20;
+  hits = 0;
+  gameFinished = false;
+  txtOutput.innerHTML = "...";
+  for (var i = 1; i < arrCells.length; i++) {
+    var cell;
+    if (i < 9) {
+      cell = "a" + i;
+    } else if (i < 17) {
+      cell = "b" + (i - 8);
+    } else if (i < 25) {
+      cell = "c" + (i - 16);
+    } else if (i < 33) {
+      cell = "d" + (i - 24);
+    } else if (i < 41) {
+      cell = "e" + (i - 32);
+    } else if (i < 49) {
+      cell = "f" + (i - 40);
+    } else if (i < 57) {
+      cell = "g" + (i - 48);
+    } else if (i <= 64) {
+      cell = "h" + (i - 56);
+    }
+    document.getElementById(cell.toString()).classList.remove("miss");
+    document.getElementById(cell.toString()).classList.remove("hit");
+    document.getElementById(cell.toString()).innerHTML = cell;
+  }
+}
+
+function checkHit(cell, id) {
+  if (gameFinished) {
+    console.log("The game is already over");
+    return;
+  };
+
+  if (id.innerHTML === "Hit" || id.innerHTML === "Miss"){
+    console.log("You already attacked there!!!");
+  } else {
+    if (cell == ship1.loc1 || cell == ship1.loc2 || cell == ship2.loc1 || cell == ship2.loc2) {
+      console.log("hit");
+      id.classList.add("hit");
+      id.innerHTML = "Hit";
+      hits++
+      guesses--
+      txtOutput.innerHTML = "Hit!";
+    } else {
+      console.log("Attacked cell " + cell + " and missed");
+      id.classList.add("miss");
+      id.innerHTML = "Miss";
+      guesses--
+      txtOutput.innerHTML = "Miss!";
+    }
+    txtGuesses.innerHTML = guesses;
+    txtHits.innerHTML = hits;
+  }
+
+  if (hits === 4) {
+    txtOutput.innerHTML = "You Win!"
+    gameFinished = true;
+  } else if (guesses === 0) {
+    txtOutput.innerHTML = "You Lose!";
+    gameFinished = true;
+  }
+}
+
+ship1.assignLoc();
+ship2.assignLoc();
+
+// displayShips(ship1.loc1);
+// displayShips(ship1.loc2);
+// displayShips(ship2.loc1);
+// displayShips(ship2.loc2);
+
+console.log(ship1.loc1, ship1.loc2);
+console.log(ship2.loc1, ship2.loc2);
+
+btnRestart.onclick = function() {
+  restartGame();
+};
---
+++ projects/battleships/attack.js
@@ -0,0 +1,74 @@
+// Each cell is given an id, upon clicking a cell, its onlcick event will call a function that is located in the main script file.
+// This seperate script file was created to keep the code clean and organised.
+
+a1.onclick = function () {checkHit(1, a1)}
+a2.onclick = function () {checkHit(2, a2)}
+a3.onclick = function () {checkHit(3, a3)}
+a4.onclick = function () {checkHit(4, a4)}
+a5.onclick = function () {checkHit(5, a5)}
+a6.onclick = function () {checkHit(6, a6)}
+a7.onclick = function () {checkHit(7, a7)}
+a8.onclick = function () {checkHit(8, a8)}
+
+b1.onclick = function () {checkHit(9, b1)}
+b2.onclick = function () {checkHit(10, b2)}
+b3.onclick = function () {checkHit(11, b3)}
+b4.onclick = function () {checkHit(12, b4)}
+b5.onclick = function () {checkHit(13, b5)}
+b6.onclick = function () {checkHit(14, b6)}
+b7.onclick = function () {checkHit(15, b7)}
+b8.onclick = function () {checkHit(16, b8)}
+
+c1.onclick = function () {checkHit(17, c1)}
+c2.onclick = function () {checkHit(18, c2)}
+c3.onclick = function () {checkHit(19, c3)}
+c4.onclick = function () {checkHit(20, c4)}
+c5.onclick = function () {checkHit(21, c5)}
+c6.onclick = function () {checkHit(22, c6)}
+c7.onclick = function () {checkHit(23, c7)}
+c8.onclick = function () {checkHit(24, c8)}
+
+d1.onclick = function () {checkHit(25, d1)}
+d2.onclick = function () {checkHit(26, d2)}
+d3.onclick = function () {checkHit(27, d3)}
+d4.onclick = function () {checkHit(28, d4)}
+d5.onclick = function () {checkHit(29, d5)}
+d6.onclick = function () {checkHit(30, d6)}
+d7.onclick = function () {checkHit(31, d7)}
+d8.onclick = function () {checkHit(32, d8)}
+
+e1.onclick = function () {checkHit(33, e1)}
+e2.onclick = function () {checkHit(34, e2)}
+e3.onclick = function () {checkHit(35, e3)}
+e4.onclick = function () {checkHit(36, e4)}
+e5.onclick = function () {checkHit(37, e5)}
+e6.onclick = function () {checkHit(38, e6)}
+e7.onclick = function () {checkHit(39, e7)}
+e8.onclick = function () {checkHit(40, e8)}
+
+f1.onclick = function () {checkHit(41, f1)}
+f2.onclick = function () {checkHit(42, f2)}
+f3.onclick = function () {checkHit(43, f3)}
+f4.onclick = function () {checkHit(44, f4)}
+f5.onclick = function () {checkHit(45, f5)}
+f6.onclick = function () {checkHit(46, f6)}
+f7.onclick = function () {checkHit(47, f7)}
+f8.onclick = function () {checkHit(48, f8)}
+
+g1.onclick = function () {checkHit(49, g1)}
+g2.onclick = function () {checkHit(50, g2)}
+g3.onclick = function () {checkHit(51, g3)}
+g4.onclick = function () {checkHit(52, g4)}
+g5.onclick = function () {checkHit(53, g5)}
+g6.onclick = function () {checkHit(54, g6)}
+g7.onclick = function () {checkHit(55, g7)}
+g8.onclick = function () {checkHit(56, g8)}
+
+h1.onclick = function () {checkHit(57, h1)}
+h2.onclick = function () {checkHit(58, h2)}
+h3.onclick = function () {checkHit(59, h3)}
+h4.onclick = function () {checkHit(60, h4)}
+h5.onclick = function () {checkHit(61, h5)}
+h6.onclick = function () {checkHit(62, h6)}
+h7.onclick = function () {checkHit(63, h7)}
+h8.onclick = function () {checkHit(64, h8)}
